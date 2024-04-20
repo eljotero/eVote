bc40a66ec4a77c288277425c31178594cb6a4e79
SzczupakKrolemPontaru <szymonwydmuch1@gmail.com>
2024-04-20 02:20:08 +0200

    EVOTE-9 add: UserServiceTests, JacCoCo dependency

---
+++ backend/src/test/java/org/evote/backend/unit/controllers/UserControllerTest.java
@@ -0,0 +1,14 @@
+package org.evote.backend.unit.controllers;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+
+}
