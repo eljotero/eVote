bf1d8f3cd2643ae31c136749c1c8ce50fd10aedf
eljotero <aleksander.janicki002@gmail.com>
2024-05-17 01:47:17 +0200

    Backend unit tests for emails

--- backend/src/main/java/org/evote/backend/controllers/EmailController.java
+++ backend/src/main/java/org/evote/backend/controllers/EmailController.java
@@ -3,7 +3,6 @@ package org.evote.backend.controllers;
 
 import org.evote.backend.config.JwtService;
 import org.evote.backend.services.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,7 +16,7 @@ public class EmailController {
     private final EmailService emailService;
     private final JwtService jwtService;
 
-    @Autowired
+
     public EmailController(EmailService emailService, JwtService jwtService) {
         this.emailService = emailService;
         this.jwtService = jwtService;
--- backend/src/main/java/org/evote/backend/users/user/exceptions/CodeAlreadySent.java
+++ backend/src/main/java/org/evote/backend/users/user/exceptions/CodeAlreadySent.java
@@ -1,6 +1,7 @@
 package org.evote.backend.users.user.exceptions;
 
 public class CodeAlreadySent extends RuntimeException {
+
     public CodeAlreadySent(String message) {
         super(message);
     }
