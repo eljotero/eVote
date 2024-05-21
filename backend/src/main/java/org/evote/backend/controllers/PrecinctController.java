6d0b163ce3165a648e9f0c614d4a24ec7e7cd2e3
eljotero <aleksander.janicki002@gmail.com>
2024-05-22 00:29:56 +0200

    fixing merge conflicts

--- backend/src/main/java/org/evote/backend/controllers/PrecinctController.java
+++ backend/src/main/java/org/evote/backend/controllers/PrecinctController.java
@@ -1,9 +1,7 @@
 package org.evote.backend.controllers;
 
-import org.evote.backend.dtos.user.PrecinctSetDTO;
 import org.evote.backend.services.PrecinctService;
 import org.evote.backend.users.precinct.entity.Precinct;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
