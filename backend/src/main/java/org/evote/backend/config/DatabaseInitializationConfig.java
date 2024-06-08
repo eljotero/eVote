0a3997a1903a2587bfb99f085f64b65750c33d7d
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2024-06-08 14:55:09 +0200

    initial scripts

--- backend/src/main/java/org/evote/backend/config/DatabaseInitializationConfig.java
+++ backend/src/main/java/org/evote/backend/config/DatabaseInitializationConfig.java
@@ -1,9 +1,12 @@
 package org.evote.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import javax.sql.DataSource;
@@ -11,22 +14,58 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseInitializationConfig {
 
-    @Value("classpath:init_votes.sql")
+    @Value("classpath:init_users.sql")
     private Resource initDataScript1;
 
+    @Value("classpath:init_votes.sql")
+    private Resource initDataScript2;
 
+    @Autowired
+    private Environment env;
 
     @Bean
-    public DataSourceInitializer dataSourceInitializer1(DataSource dataSource1) {
+    public DataSource usersDataSourceInit() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.users.driverClassName"));
+        dataSource.setUrl(env.getProperty("spring.datasource.users.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.users.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.users.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public DataSource votesDataSourceInit() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.votes.driverClassName"));
+        dataSource.setUrl(env.getProperty("spring.datasource.votes.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.votes.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.votes.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer1() {
         ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
         databasePopulator.addScript(initDataScript1);
 
         DataSourceInitializer initializer = new DataSourceInitializer();
-        initializer.setDataSource(dataSource1);
+        initializer.setDataSource(usersDataSourceInit());
         initializer.setDatabasePopulator(databasePopulator);
 
         return initializer;
     }
 
+    @Bean
+    public DataSourceInitializer dataSourceInitializer2() {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.addScript(initDataScript2);
 
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(votesDataSourceInit());
+        initializer.setDatabasePopulator(databasePopulator);
+
+        return initializer;
+    }
 }
---
+++ backend/src/main/resources/init_users.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO address (address_id, address_line, city, country, voivodeship, zip_code)
+VALUES (1,'Gowno', 'Brzeziny', 'Polska', 'Łódzkie', '12-000');
+
+INSERT INTO address (address_id, address_line, city, country, voivodeship, zip_code)
+VALUES (2,'Gowno', 'Brzeziny', 'Polska', 'Łódzkie', '12-000');
+
+INSERT INTO address (address_id, address_line, city, country, voivodeship, zip_code)
+VALUES (3,'Gowno', 'Brzeziny', 'Polska', 'Łódzkie', '12-000');
