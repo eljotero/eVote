5d0c594539e8f7ff09a7875c4d6481302e97cc63
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2024-06-18 17:33:59 +0200

    add tests and last statistics

--- backend/src/main/java/org/evote/backend/controllers/StatisticsController.java
+++ backend/src/main/java/org/evote/backend/controllers/StatisticsController.java
@@ -29,25 +29,4 @@ public class StatisticsController {
         allResults.put("resultsByCityType", statisticsService.getResultsByCityType(electionId));
         return ResponseEntity.ok(allResults);
     }
-    @GetMapping("/detailedResults/{electionId}")
-    public ResponseEntity<?> getDetailedResults(@PathVariable int electionId) {
-        return ResponseEntity.ok(statisticsService.getDetailedResults(electionId));
-    }
-
-    @GetMapping("/detailedVotesByParty/{electionId}/{politicalPartyId}")
-    public ResponseEntity<?> getDetailedVotesByParty(@PathVariable int electionId, @PathVariable int politicalPartyId) {
-        return ResponseEntity.ok(statisticsService.getDetailedVotesByParty(electionId, politicalPartyId));
-    }
-
-    @GetMapping("/detailedEducationVotesByParty/{electionId}/{politicalPartyId}")
-    public ResponseEntity<?> getDetailedEducationVotesByParty(@PathVariable int electionId, @PathVariable int politicalPartyId) {
-        Map<String, Map<String, Integer>> detailedEducationVotes = statisticsService.getDetailedEducationVotesByParty(electionId, politicalPartyId);
-        return ResponseEntity.ok(detailedEducationVotes);
-    }
-
-    @GetMapping("/detailedCityTypeVotesByParty/{electionId}/{politicalPartyId}")
-    public ResponseEntity<?> getDetailedCityTypeVotesByParty(@PathVariable int electionId, @PathVariable int politicalPartyId) {
-        Map<String, Map<String, Integer>> detailedCityTypeVotes = statisticsService.getDetailedCityTypeVotesByParty(electionId, politicalPartyId);
-        return ResponseEntity.ok(detailedCityTypeVotes);
-    }
 }
