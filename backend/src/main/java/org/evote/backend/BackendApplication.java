5fd5f48ce0231fc7897b2234ed8fa9394a331fc4
eljotero <aleksander.janicki002@gmail.com>
2024-06-23 01:08:24 +0200

    Added swagger

--- backend/src/main/java/org/evote/backend/BackendApplication.java
+++ backend/src/main/java/org/evote/backend/BackendApplication.java
@@ -1,9 +1,26 @@
 package org.evote.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "E-Vote API",
+                version = "1.0",
+                description = "E-Vote API for managing elections and voting",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "E-Vote Team",
+                        email = "evote.info.support@gmail.com"
+                ),
+                license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        )
+)
 public class BackendApplication {
 
     public static void main(String[] args) {
